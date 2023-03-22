16c71f4206d8cfe60dd1a8230adc0978bd4c8dca
Kaosi <begbuonu@gmail.com>
2023-03-22 17:32:07 +0100

    modified mutation.py

--- phase3/mutation.py
+++ phase3/mutation.py
@@ -1,9 +1,10 @@
 """
 individuals obey the following representation:
 a dictionary consisting of several lists, one for each worker and containing their assigned tasks
-{"W1":[T1, T5, ...], "W2": [T0, T16, T10], ..., "W10": [T9]}
+{'W1': [5, 1], 'W2': [0, 16, 10], 'W3': [9]}
 """
 import random
+import numpy as np
 
 
 # mutation strategies:
@@ -13,23 +14,108 @@ import random
 # add undone task to a worker
 # remove a task from a worker
 
-def mutate_exchange(individual):
-    # Melanie implements this
-    pass
+def mutate_flip(individual_ini, data):
+    # define time matrix as global variable
+    t = data.t
 
+    individual = individual_ini.copy()
 
-def mutate_reassign(individual):
-    # ToDo: add probabilities depending on the length of a worker's task list
-    num_workers = len(individual)
-    worker_IDs = random.sample(range(num_workers), 2)
-    worker1 = worker_IDs[0]
-    worker2 = worker_IDs[1]
-    task = individual[worker1].pop(random.randrange(len(individual[worker1])))
-    individual[worker2].append(task)
+    # choose a random worker
+    w1, w2 = random.sample(list(individual.keys()), 2)
+
+    good_task_w1 = data.TasksW[w1]
+    good_task_w2 = data.TasksW[w2]
+
+    # choose random task to flip
+    idx1 = random.randint(0, len(individual[w1]) - 1)
+    idx2 = random.randint(0, len(individual[w2]) - 1)
+
+    # dist_mat1 = [t[data.Houses[w1]][task] if task in good_task_w2 else 0 for task in individual[w1]]
+    # dist_mat2 = [t[data.Houses[w2]][task] if task in good_task_w1 else 0 for task in individual[w2]]
+    # print(dist_mat2)
+
+    # if sum(dist_mat1) == 0 or sum(dist_mat2) == 0:
+    #     # we make no changes if there is no posibility of change
+    #     print('AHHHHHHHHHH')
+    #     return individual
+
+    # l_a = list(range(len(individual[w1])))
+
+    # idx1 = random.choices(list(range(len(individual[w1]))), weights=dist_mat1)[0]
+    # idx2 = random.choices(list(range(len(individual[w2]))), weights=dist_mat2)[0]
+
+    # print(idx1, idx2)
+
+    # Create distance Matrix
+    dist_mat1 = np.cumsum([t[data.Houses[w1]][task] if task in good_task_w2 else 0 for task in individual[w1]])
+    dist_mat2 = np.cumsum([t[data.Houses[w2]][task] if task in good_task_w1 else 0 for task in individual[w2]])
+
+    if dist_mat1[-1] == 0 or dist_mat2[-1] == 0:
+        # we make no changes
+        return individual
+
+    dist_mat1 = dist_mat1 / dist_mat1[-1]
+    dist_mat2 = dist_mat2 / dist_mat2[-1]
+
+    alpha1 = random.random();
+    alpha2 = random.random()
+
+    comparation1 = list(dist_mat1 < alpha1) + [False]
+    comparation2 = list(dist_mat2 < alpha2) + [False]
+
+    idx1 = 0
+    idx2 = 0
+    while comparation1[idx1]:
+        idx1 += 1
+    while comparation2[idx2]:
+        idx2 += 1
+
+    # flip task
+    individual[w1][idx1], individual[w2][idx2] = individual[w2][idx2], individual[w1][idx1]
+
     return individual
 
 
-def mutate_reorder(individual):
+def mutate_reassign(individual, data):
+    workers = list(individual)
+    # use the number of tasks of a worker as their probability to be affected by reassignment
+    # a worker is more likely to be chosen if more tasks are allocated to them
+    probs1 = [len(individual[worker]) for worker in workers]
+
+    # pick a worker from which a task is taken
+    worker1 = random.choices(workers, weights=probs1, k=1)
+
+    # adjust probs
+    # a worker is more likely to be chosen if less tasks are allocated to them
+    workers.remove(worker1)
+    probs2 = [1 / (1 + len(individual[worker])) for worker in workers]
+
+    # pick another worker who gets an additional task
+    worker2 = random.choices(workers, weights=probs2, k=1)
+
+    # check whether the task can be done by worker 2...
+    check = True
+    cnt = 0
+    tasks = individual[worker1]
+    # ... as long as a suitable task has been found or there is no task left in tasks
+    while check and cnt < len(individual[worker1]):
+        # take a random task from worker 1
+        task = tasks.pop(random.randrange(len(tasks)))
+
+        # check whether task can be done by worker 2
+        if task in data.TasksW[worker2]:
+            #  reassign the task to worker 2 at a random place
+            individual[worker1].remove(task)
+            individual[worker2].insert(random.randrange(len(individual[worker2])+1), task)
+
+            check = False
+
+        cnt += 1
+
+    return individual
+
+
+def mutate_reorder(individual, data=None):
     num_workers = len(individual)
     worker = random.randrange(num_workers)
     tasks = individual[worker]
@@ -50,14 +136,31 @@ def swapPositions(list, pos1, pos2):
     return list
 
 
-def mutate_remove(individual):
-    # ToDo: add probabilities depending on the length of a worker's task list
-    num_workers = len(individual)
-    worker = random.randrange(num_workers)
+def mutate_remove(individual, data):
+    workers = list(individual)
+    # use the number of tasks of a worker as their probability to be affected by reassignment
+    # a worker is more likely to be chosen if more tasks are allocated to them
+    probs_worker = [len(individual[worker]) for worker in workers]
+
+    # pick a worker from which a task is taken
+    worker = random.choices(workers, weights=probs_worker, k=1)
+
     tasks = individual[worker]
-    task = random.randrange(len(tasks))
-    tasks.pop(task)
+
+    time_matrix = data.t    # access time matrix by e.g. data.t[][] with task or data.Houses[worker]
+    probs_task = []
+    for task_ID in range(len(tasks)):
+        if task_ID == 0:
+            probs_task.append()
+        elif task_ID == len(tasks)-1:
+            probs_task.append()
+        else:
+            probs_task.append()
+
+    task_ID = random.randrange(len(tasks))
+    tasks.pop(task_ID)
     individual[worker] = tasks
+
     return individual
 
 
@@ -69,3 +172,4 @@ def mutate_insert(individual, task):
     tasks.insert(pos, task)
     individual[worker] = tasks
     return individual
+
