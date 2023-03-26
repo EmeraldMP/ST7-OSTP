65f955621a220c650fda28d7811fdddcd984e9d3
Kaosi <begbuonu@gmail.com>
2023-03-26 01:43:04 +0100

    minor changes in main and mutation

--- phase3/main.py
+++ phase3/main.py
@@ -31,13 +31,20 @@ def main(endroit, instance):
 
     data = Data(endroit, instance)
 
+    initial_population_number = 10
+    best_future_generation_number = 10
+    number_of_copies = 10
+    stop_cost = 1
+    best_solution = None
+
     individuals = create_population(data, initial_population_number)
     optimum = False
     while not optimum:
         future_generation = individuals_copy(individuals, number_of_copies)
         for individual in future_generation:
             new_individual = mutate(individual, data)
-            future_generation.remove(individual)
+            # do not remove old generation! maybe some old individuals are better than the new ones
+            # future_generation.remove(individual)
             future_generation.append(new_individual)
         # calculate the cost values, select the best ones
         best_new_individuals = select_best_group(
@@ -47,17 +54,13 @@ def main(endroit, instance):
         # we cant find a neighbourhood or something like that, it will
         # depend on the metaheuristic used I guess (VNS, simulated annealing, etc)
         best_solution = find_best_solution(best_new_individuals)
-        if calculate_cost(best_solution) <= stop_cost:
+        if calculate_cost(data, best_solution) <= stop_cost:
             optimum = True
 
     return best_solution
 
 
 if __name__ == '__main__':
-    initial_population_number = 10
-    best_future_generation_number = 10
-    number_of_copies = 10
-    stop_cost = 1
     endroit = ""
     instance = ""
 
